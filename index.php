3d348776227635ab743764658265d493ac39e8f3
David Mather <davidmather@live.ie>
2016-02-03 21:11:56 +0000

    a

--- index.php
+++ index.php
@@ -57,27 +57,18 @@ $content = file_get_contents($page);
 $array = array_unique($array);
 
     date_default_timezone_set( 'Europe/Dublin' ); //eircom
-    $from_email = 'davidmather@live.ie'; //sender email
+    $from_email = 'youremail'; //sender email
    
     $subject = 'Job Application'; //subject of email
-    $message = file_get_contents("cv3.html"); //message body
+    $message = file_get_contents("htmlemailfile"); //message body
    
-    //get file details we need
-    $file_tmp_name    = $_FILES['my_file']['tmp_name'];
-    $file_name        = $_FILES['my_file']['name'];
-    $file_size        = $_FILES['my_file']['size'];
-    $file_type        = $_FILES['my_file']['type'];
-    $file_error       = $_FILES['my_file']['error'];
    
-    $user_email = filter_var($_POST["email"], FILTER_SANITIZE_EMAIL);
+    $file="filename";
+    $file_type=filetype($file);
+    $file_size=filesize($file);
 
-    if($file_error>0)
-    {
-        die('upload error');
-    }
-    
     //read from the uploaded file & base64_encode content for the mail
-    $handle = fopen($file_tmp_name, "r");
+    $handle = fopen($file, "r");
     $content = fread($handle, $file_size);
     fclose($handle);
     $encoded_content = chunk_split(base64_encode($content));
@@ -88,7 +79,7 @@ $array = array_unique($array);
         //header
         $headers = "MIME-Version: 1.0\r\n";
         $headers .= "From:".$from_email."\r\n";
-        $headers .= "Reply-To: ".$user_email."" . "\r\n";
+        $headers .= "Reply-To: ".$from_email."" . "\r\n";
         $headers .= "Content-Type: multipart/mixed; boundary = {$boundary}\r\n\r\n";
        
         //plain text
